0841a46cc414568573b3ce761938e557c671ae9d
NightFish <NightFish.net@gmail.com>
2023-02-09 19:12:12 +1300

    Add dependency injection template

--- src/Freshup/Program.cs
+++ src/Freshup/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-Console.WriteLine("Hello, World!");
+namespace Freshup;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        // Create generic host and use startup service.
+        // We use a startup service here instead of just adding each individual service
+        // directly since some services require setup with things like configuration options.
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureServices((_, services) =>
+                services.AddHostedService<Startup>())
+            .ConfigureAppConfiguration((context, builder) =>
+                builder.AddUserSecrets<Program>());
+    }
+}
---
+++ src/Freshup/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Freshup;
+
+public class Startup : IHostedService
+{
+    private readonly IConfiguration _configuration;
+    private IEnumerable<IHostedService> _runningServices;
+        
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(_configuration)
+            .CreateLogger();
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        Log.Information("Application started.");
+
+        // Set up services for dependency injection.
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        var provider = services.BuildServiceProvider();
+            
+        _runningServices = provider.GetServices<IHostedService>();
+        return Task.WhenAll(_runningServices.Select(s => 
+            s.StartAsync(cancellationToken)));
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        Log.Information("Application stopping.");
+        return Task.WhenAll(_runningServices.Select(s => 
+            s.StopAsync(cancellationToken)));
+    }
+        
+    /// <summary>
+    /// Configures services to be dependency injected.
+    /// </summary>
+    private void ConfigureServices(IServiceCollection services)
+    {
+        services.AddLogging(x => x.AddSerilog());
+    }
+}
