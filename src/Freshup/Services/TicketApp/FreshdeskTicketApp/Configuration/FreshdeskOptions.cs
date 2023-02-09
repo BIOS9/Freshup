525aec0aee48477e6bab84cf8fb65cd251c637f5
NightFish <NightFish.net@gmail.com>
2023-02-09 19:26:17 +1300

    Add freshdesk config options

---
+++ src/Freshup/Services/TicketApp/FreshdeskTicketApp/Configuration/FreshdeskOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Freshup.Services.FreshdeskTicketApp.Configuration;
+
+public class FreshdeskOptions
+{
+    public const string Name = "Freshdesk";
+
+    public string ApiKey { get; set; } = string.Empty;
+}
---
+++ src/Freshup/Services/TicketApp/FreshdeskTicketApp/Configuration/FreshdeskOptionsValidation.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Freshup.Services.FreshdeskTicketApp.Configuration;
+
+public class FreshdeskOptionsValidation : IValidateOptions<FreshdeskOptions>
+{
+    public ValidateOptionsResult Validate(string name, FreshdeskOptions options)
+    {
+        if (string.IsNullOrWhiteSpace(options.ApiKey))
+        {
+            return ValidateOptionsResult.Fail("Missing API key.");
+        }
+
+        return ValidateOptionsResult.Success;
+    }
+}
