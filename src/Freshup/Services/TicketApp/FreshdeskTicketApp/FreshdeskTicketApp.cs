3ee85b9c250ed20b9b538c11b10f101cee906245
NightFish <NightFish.net@gmail.com>
2023-02-10 16:52:04 +1300

    Add fundamental ticket polling system

--- src/Freshup/Services/TicketApp/FreshdeskTicketApp/FreshdeskTicketApp.cs
+++ src/Freshup/Services/TicketApp/FreshdeskTicketApp/FreshdeskTicketApp.cs
@@ -1,6 +1,9 @@
 ﻿using FreshdeskApi.Client;
+using FreshdeskApi.Client.Tickets.Requests;
 using Freshup.Services.FreshdeskTicketApp.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using TimeSpanParserUtil;
 
 namespace Freshup.Services.FreshdeskTicketApp;
 
@@ -9,24 +12,75 @@ public class FreshdeskTicketApp : ITicketApp
     private readonly FreshdeskOptions _options;
     private readonly ILogger<FreshdeskTicketApp> _logger;
     private readonly FreshdeskClient _freshdeskClient;
-    
-    public FreshdeskTicketApp(FreshdeskOptions options, ILogger<FreshdeskTicketApp> logger)
+    private readonly CancellationTokenSource _pollingTokenSource = new CancellationTokenSource();
+    private HashSet<HashableTicket> _tickets = new HashSet<HashableTicket>();
+
+    public FreshdeskTicketApp(IOptions<FreshdeskOptions> options, ILogger<FreshdeskTicketApp> logger)
     {
-        _options = options ?? throw new ArgumentNullException(nameof(options));
+        _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         
-        using var freshdeskHttpClient = FreshdeskHttpClient.Create(options.Domain, options.ApiKey);
+        var freshdeskHttpClient = FreshdeskHttpClient.Create(_options.Domain, _options.ApiKey);
         _freshdeskClient = FreshdeskClient.Create(freshdeskHttpClient);
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
+        StartPolling(_pollingTokenSource.Token);
         _logger.LogInformation("Freshdesk monitoring started");
         return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
+        _pollingTokenSource.Cancel();
+        return Task.CompletedTask;
+    }
+
+    public Task<ITicket> GetTicketsAsync()
+    {
         throw new NotImplementedException();
+    }
+
+    private async void StartPolling(CancellationToken cancellationToken)
+    {
+        #if !DEBUG
+        await Task.Delay(TimeSpan.FromSeconds(30));
+        #endif
+
+        bool firstRun = true;
+        
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                var existingTickets = _tickets; // Defensive reference
+                var newTickets = new HashSet<HashableTicket>();
+                
+                await foreach (var ticket in _freshdeskClient.Tickets.ListAllTicketsAsync(
+                                   new ListAllTicketsRequest(),
+                                   new PaginationConfiguration(),
+                                   cancellationToken))
+                {
+                    var hashTicket = new HashableTicket(ticket);
+
+                    if (!firstRun && !existingTickets.Contains(hashTicket))
+                    {
+                        _logger.LogInformation("NEW TICKET! {Subject}", ticket.Subject);
+                    }
+
+                    newTickets.Add(hashTicket);
+                }
+
+                _tickets = newTickets;
+                firstRun = false;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occured in ticket polling loop {Message}", ex.Message);
+                await Task.Delay(TimeSpan.FromMinutes(5), cancellationToken);
+            }
+            await Task.Delay(TimeSpanParser.Parse(_options.TicketPollInterval));
+        }
     }
 }
---
+++ src/Freshup/Services/TicketApp/FreshdeskTicketApp/HashableTicket.cs
@@ -0,0 +1,30 @@
+﻿using FreshdeskApi.Client.Tickets.Models;
+
+namespace Freshup.Services.FreshdeskTicketApp;
+
+public class HashableTicket
+{
+    public Ticket Ticket { get; }
+
+    public HashableTicket(Ticket ticket)
+    {
+        Ticket = ticket ?? throw new ArgumentNullException(nameof(ticket));
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null) return false;
+        if (obj.GetType() != GetType()) return false;
+        return ((HashableTicket)obj).Ticket.Id == Ticket.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Ticket?.Id.GetHashCode() ?? 0;
+    }
+
+    public override string ToString()
+    {
+        return Ticket.ToString();
+    }
+}
