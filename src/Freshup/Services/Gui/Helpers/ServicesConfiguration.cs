f7fecf92fe8c9d85bb01badff682693803e88ae2
NightFish <NightFish.net@gmail.com>
2023-02-11 22:33:23 +1300

    Add winforms gui

---
+++ src/Freshup/Services/Gui/Helpers/ServicesConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Freshup.Services.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Freshup.Services.Gui.Helpers;
+
+public static class ServicesConfiguration
+{
+    public static void AddGui(this IServiceCollection services)
+    {
+        services.AddSingleton<MainForm>();
+    }
+}
---
+++ src/Freshup/Services/Gui/MainForm.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using Microsoft.Extensions.Logging;
+
+namespace Freshup.Services.Testing;
+
+public partial class MainForm : Form
+{
+    private readonly ILogger<Testing> _logger;
+    private readonly ITicketApp _ticketApp;
+    
+    public MainForm(ILogger<Testing> logger, ITicketApp ticketApp)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _ticketApp = ticketApp ?? throw new ArgumentNullException(nameof(ticketApp));
+        InitializeComponent();
+    }
+}
