7a2329a3c9993ced253df6ec91fa6b447ccb397f
NightFish <NightFish.net@gmail.com>
2023-02-11 22:59:58 +1300

    Add basic layout for some settings controls and add notification form

--- src/Freshup/Services/Gui/MainForm.Designer.cs
+++ src/Freshup/Services/Gui/MainForm.Designer.cs
@@ -31,11 +31,132 @@ partial class MainForm
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "MainForm";
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.enableNotificationsCheckBox = new System.Windows.Forms.CheckBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.testNotificationButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.testNotificationButton);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.trackBar2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.enableNotificationsCheckBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(414, 306);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ticket Notifications";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Auto hide notifications";
+            // 
+            // enableNotificationsCheckBox
+            // 
+            this.enableNotificationsCheckBox.AutoSize = true;
+            this.enableNotificationsCheckBox.Location = new System.Drawing.Point(6, 22);
+            this.enableNotificationsCheckBox.Name = "enableNotificationsCheckBox";
+            this.enableNotificationsCheckBox.Size = new System.Drawing.Size(187, 19);
+            this.enableNotificationsCheckBox.TabIndex = 0;
+            this.enableNotificationsCheckBox.Text = "Enable new ticket notifications";
+            this.enableNotificationsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(140, 47);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(199, 45);
+            this.trackBar1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ticket polling interval";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(140, 89);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(199, 45);
+            this.trackBar2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Default notification sound";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(432, 16);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(388, 302);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ticket Snoozing";
+            // 
+            // testNotificationButton
+            // 
+            this.testNotificationButton.Location = new System.Drawing.Point(6, 180);
+            this.testNotificationButton.Name = "testNotificationButton";
+            this.testNotificationButton.Size = new System.Drawing.Size(111, 23);
+            this.testNotificationButton.TabIndex = 7;
+            this.testNotificationButton.Text = "Test Notification";
+            this.testNotificationButton.UseVisualStyleBackColor = true;
+            this.testNotificationButton.Click += new System.EventHandler(this.testNotificationButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 332);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "Freshup";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+
     }
 
     #endregion
+
+    private GroupBox groupBox1;
+    private Label label1;
+    private CheckBox enableNotificationsCheckBox;
+    private Label label3;
+    private TrackBar trackBar2;
+    private Label label2;
+    private TrackBar trackBar1;
+    private GroupBox groupBox2;
+    private Button testNotificationButton;
 }
--- src/Freshup/Services/Gui/MainForm.cs
+++ src/Freshup/Services/Gui/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Forms;
+using Freshup.Services.Gui;
 using Microsoft.Extensions.Logging;
 
 namespace Freshup.Services.Testing;
@@ -7,11 +8,18 @@ public partial class MainForm : Form
 {
     private readonly ILogger<MainForm> _logger;
     private readonly ITicketApp _ticketApp;
-    
+    private readonly NotificationForm _notificationForm;
+
     public MainForm(ILogger<MainForm> logger, ITicketApp ticketApp)
     {
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         _ticketApp = ticketApp ?? throw new ArgumentNullException(nameof(ticketApp));
+        _notificationForm = new NotificationForm();
         InitializeComponent();
+    }
+
+    private void testNotificationButton_Click(object sender, EventArgs e)
+    {
+
     }
 }
