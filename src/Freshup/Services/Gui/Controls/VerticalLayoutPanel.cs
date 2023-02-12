f45cc0f5196a2d70a55669be0ccb0e5d1cbe182c
NightFish <NightFish.net@gmail.com>
2023-02-12 13:55:28 +1300

    Add vertical layout panel control

---
+++ src/Freshup/Services/Gui/Controls/VerticalLayoutPanel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Freshup.Services.Gui.Controls
+{
+    public partial class VerticalLayoutPanel : FlowLayoutPanel
+    {
+        public VerticalLayoutPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void organise()
+        {
+            foreach (Control c in Controls)
+            {
+                if (VerticalScroll.Visible)
+                    c.Width = Width - SystemInformation.VerticalScrollBarWidth;
+                else
+                    c.Width = Width;
+            }
+        }
+
+        private void VerticalLayoutPanel_ControlAdded(object sender, ControlEventArgs e)
+        {
+            organise();
+        }
+
+        private void VerticalLayoutPanel_ControlRemoved(object sender, ControlEventArgs e)
+        {
+            organise();
+        }
+
+        private void VerticalLayoutPanel_Resize(object sender, EventArgs e)
+        {
+            organise();
+        }
+    }
+}
