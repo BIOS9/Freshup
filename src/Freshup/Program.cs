193c9971710877fa4088d09055a72d80194a9665
NightFish <NightFish.net@gmail.com>
2023-02-13 21:23:39 +1300

    Remove startup file

--- src/Freshup/Program.cs
+++ src/Freshup/Program.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Squirrel.Sources;
+﻿using Squirrel.Sources;
 using Squirrel;
+using Freshup.Services.Gui;
 
 namespace Freshup;
 
@@ -11,6 +9,48 @@ public class Program
     [STAThread]
     static void Main(string[] args)
     {
+        CheckUpdates();
         ApplicationConfiguration.Initialize();
+        new Gui().Run();
     }
+
+    #region UPDATES
+
+    private static void CheckUpdates()
+    {
+        SquirrelAwareApp.HandleEvents(
+            onInitialInstall: OnAppInstall,
+            onAppUninstall: OnAppUninstall,
+            onEveryRun: OnAppRun);
+
+        using (var mgr = new UpdateManager(new GithubSource("https://github.com/BIOS9/Freshup", string.Empty, false)))
+        {
+            if (mgr.IsInstalledApp)
+            {
+                var newVersion = mgr.UpdateApp().Result;
+
+                if (newVersion != null)
+                {
+                    UpdateManager.RestartApp();
+                }
+            }
+        }
+    }
+
+    private static void OnAppInstall(SemanticVersion version, IAppTools tools)
+    {
+        tools.CreateShortcutForThisExe(ShortcutLocation.StartMenu);
+    }
+
+    private static void OnAppUninstall(SemanticVersion version, IAppTools tools)
+    {
+        tools.RemoveShortcutForThisExe(ShortcutLocation.StartMenu);
+    }
+
+    private static void OnAppRun(SemanticVersion version, IAppTools tools, bool firstRun)
+    {
+        tools.SetProcessAppUserModelId();
+    }
+
+    #endregion
 }
