9d66f8724c8806cad852ae2aba86593630a70645
NightFish <NightFish.net@gmail.com>
2023-02-14 13:45:39 +1300

    Remove tray icon from notification form

--- src/Freshup/Gui/NotificationForm.Designer.cs
+++ src/Freshup/Gui/NotificationForm.Designer.cs
@@ -28,13 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotificationForm));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.closeButton1 = new Freshup.Services.Gui.CloseButton();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.ticketPanel = new Freshup.Services.Gui.Controls.VerticalLayoutPanel();
-            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -43,7 +41,7 @@
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.pictureBox1.Image = Properties.Resources.NotificationImage;
+            this.pictureBox1.Image = global::Freshup.Properties.Resources.NotificationImage;
             this.pictureBox1.Location = new System.Drawing.Point(2, 54);
             this.pictureBox1.MaximumSize = new System.Drawing.Size(162, 215);
             this.pictureBox1.Name = "pictureBox1";
@@ -64,7 +62,7 @@
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.Image = Properties.Resources.Thirsty;
+            this.pictureBox2.Image = global::Freshup.Properties.Resources.Thirsty;
             this.pictureBox2.Location = new System.Drawing.Point(4, 4);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(137, 44);
@@ -82,12 +80,6 @@
             this.ticketPanel.Name = "ticketPanel";
             this.ticketPanel.Size = new System.Drawing.Size(453, 138);
             this.ticketPanel.TabIndex = 4;
-            // 
-            // notifyIcon1
-            // 
-            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "Freshup";
-            this.notifyIcon1.Visible = true;
             // 
             // NotificationForm
             // 
@@ -125,6 +117,5 @@
         private CloseButton closeButton1;
         private PictureBox pictureBox2;
         private Controls.VerticalLayoutPanel ticketPanel;
-        private NotifyIcon notifyIcon1;
     }
 }
